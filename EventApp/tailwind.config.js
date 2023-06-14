a311bc2d25c374c13dcae1dd4b6c31aa22346f17
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-06-14 15:33:31 +0700

    Footer

--- EventApp/tailwind.config.js
+++ EventApp/tailwind.config.js
@@ -1,8 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {}
+  corePlugins: {
+    container: false
   },
-  plugins: []
+  theme: {
+    extend: {
+      colors: {
+        blueFooterOne: '#0173B5',
+        blueFooterTwo: '#4F89C1',
+        blue: '#0073B5',
+        gray: '#4c4c4c'
+      }
+    }
+  },
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    })
+  ]
 }
