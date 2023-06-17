4a581632b4833c3c7b98e5791995b7bf96ada133
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-06-18 00:01:02 +0700

    Custom page Testimonials Completed

--- EventApp/src/components/Carousels/CarouselTestimonials/CarouselCategoryPost.tsx
+++ EventApp/src/components/Carousels/CarouselTestimonials/CarouselCategoryPost.tsx
@@ -1,46 +1,71 @@
 import { useState, useEffect } from 'react'
-import postOne from '../../../assets/images/Testimonials/postOne.png'
 
-const slides = [`${postOne}`, `${postOne}`, `${postOne}`, `${postOne}`]
-
+const slides = [
+  {
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facere commodi vitae! Voluptatum dolor deleniti sequi ut aperiam hic, ipsam laudantium consequuntur quas magnam unde nam expedita aspernatur praesentium eveniet?',
+    name: 'Quynh Dang - CEO'
+  },
+  {
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facere commodi vitae! Voluptatum dolor deleniti sequi ut aperiam hic, ipsam laudantium consequuntur quas magnam unde nam expedita aspernatur praesentium eveniet?',
+    name: 'Hai Hoai - CEO'
+  },
+  {
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facere commodi vitae! Voluptatum dolor deleniti sequi ut aperiam hic, ipsam laudantium consequuntur quas magnam unde nam expedita aspernatur praesentium eveniet?',
+    name: 'Hoang Ha - CEO'
+  },
+  {
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facere commodi vitae! Voluptatum dolor deleniti sequi ut aperiam hic, ipsam laudantium consequuntur quas magnam unde nam expedita aspernatur praesentium eveniet?',
+    name: 'Huy Hoang - CEO'
+  }
+]
 export default function CarouselCategoryPost() {
   const autoSlide = true
   const autoSlideInterval = 3000
-  const [curr, setCurr] = useState(0)
 
-  // const prev = () => setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1))
-  const next = () => setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
-  const handleClick = (index: number) => {
-    setCurr((curr) => (curr === index ? slides.length - (slides.length - index) + 1 : index))
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const nextSlide = () => {
+    setCurrentIndex((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
   }
+
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex((curr) => (curr === slideIndex ? slides.length - (slides.length - slideIndex) + 1 : slideIndex))
+  }
+
   useEffect(() => {
     if (!autoSlide) return
-    const slideInterval = setInterval(next, autoSlideInterval)
+    const slideInterval = setInterval(nextSlide, autoSlideInterval)
     return () => clearInterval(slideInterval)
   }, [autoSlide])
   return (
     <>
-      <div className='relative overflow-hidden'>
-        <div
-          className='flex transition-transform duration-700 ease-out'
-          style={{ transform: `translateX(-${curr * 100}%)` }}
-        >
-          {slides.map((element, index) => (
-            <img src={element} alt='' key={index} className='z-[-999999]' />
-          ))}
-        </div>
-        <div className='absolute bottom-10 left-0 right-0 sm:bottom-14 md:bottom-20 lg:bottom-28'>
-          <div className='flex items-center justify-center gap-1'>
-            {slides.map((_, index) => (
-              <button
-                key={index}
-                className={`
-          h-1 w-3 bg-gray transition-all
-          ${curr === index ? 'w-5' : 'bg-opacity-30'}`}
-                onClick={() => handleClick(index)}
-              />
-            ))}
+      <div className='group relative m-auto h-[50vh] w-full max-w-[1400px] px-0 py-8 md:px-4 md:py-16'>
+        <div className='flex h-full w-full items-center px-10 text-center transition-all duration-1000 ease-out md:px-32'>
+          <div>
+            <div className='mb-4 flex justify-center'>
+              <svg xmlns='http://www.w3.org/2000/svg' height='3em' viewBox='0 0 448 512'>
+                <path d='M448 296c0 66.3-53.7 120-120 120h-8c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H320c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72zm-256 0c0 66.3-53.7 120-120 120H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72z' />
+              </svg>
+            </div>
+            <div>{slides[currentIndex].description}</div>
+            <h4 className='mt-4 text-sm uppercase'> {slides[currentIndex].name}</h4>
           </div>
+        </div>
+
+        <div className='flex items-center justify-center gap-1'>
+          {slides.map((_, slideIndex) => (
+            <button
+              key={slideIndex}
+              className={`
+          h-1 w-3 bg-gray transition-all
+          ${currentIndex === slideIndex ? 'w-5' : 'bg-opacity-30'}`}
+              onClick={() => goToSlide(slideIndex)}
+            />
+          ))}
         </div>
       </div>
     </>
--- EventApp/src/components/Carousels/CarouselTestimonials/CarouselReviews.tsx
+++ EventApp/src/components/Carousels/CarouselTestimonials/CarouselReviews.tsx
@@ -1,50 +1,92 @@
-import { useState } from 'react'
-import postOne from '../../../assets/images/Testimonials/postOne.png'
+import { useState, useEffect } from 'react'
 
-const slides = [`${postOne}`, `${postOne}`, `${postOne}`, `${postOne}`]
-export default function CarouselReviews() {
-  const [curr, setCurr] = useState(0)
+const slides = [
+  {
+    testimonial: 'Bridegerton Concept',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facere commodi vitae! Voluptatum dolor deleniti sequi ut aperiam hic, ipsam laudantium consequuntur quas magnam unde nam expedita aspernatur praesentium eveniet?',
+    name: 'Hoang Nhu - Out valued bride'
+  },
+  {
+    testimonial: 'Rapunzel Concept',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facere commodi vitae! Voluptatum dolor deleniti sequi ut aperiam hic, ipsam laudantium consequuntur quas magnam unde nam expedita aspernatur praesentium eveniet?',
+    name: 'Hai Hoai - Out valued bride'
+  },
+  {
+    testimonial: 'Beaty Concept',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facere commodi vitae! Voluptatum dolor deleniti sequi ut aperiam hic, ipsam laudantium consequuntur quas magnam unde nam expedita aspernatur praesentium eveniet?',
+    name: 'Hoang Ha - Out valued bride'
+  },
+  {
+    testimonial: 'Elegant Concept',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facere commodi vitae! Voluptatum dolor deleniti sequi ut aperiam hic, ipsam laudantium consequuntur quas magnam unde nam expedita aspernatur praesentium eveniet?',
+    name: 'Huy Hoang - Out valued bride'
+  }
+]
 
-  const prev = () => setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1))
-  const next = () => setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
+export default function CarouselCategoryPost() {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const autoSlide = true
+  const autoSlideInterval = 5000
+
+  const prevSlide = () => {
+    setCurrentIndex((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
+  }
+
+  const nextSlide = () => {
+    setCurrentIndex((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
+  }
+  useEffect(() => {
+    if (!autoSlide) return
+    const slideInterval = setInterval(nextSlide, autoSlideInterval)
+    return () => clearInterval(slideInterval)
+  }, [autoSlide])
 
   return (
     <>
-      <div className='relative overflow-hidden'>
-        <div
-          className='flex transition-transform duration-700 ease-out'
-          style={{ transform: `translateX(-${curr * 100}%)` }}
+      <div className='group relative m-auto h-[50vh] w-full max-w-[1400px] px-0 py-8 md:px-4 md:py-16'>
+        <div className='flex h-full w-full items-center px-10 text-center transition-all duration-1000 ease-out md:px-32'>
+          <div>
+            <h2 className='my-4 text-lg font-[550] uppercase text-blue md:text-3xl '>
+              {slides[currentIndex].testimonial}
+            </h2>
+            <div>{slides[currentIndex].description}</div>
+            <h4 className='mt-4 text-sm uppercase'> {slides[currentIndex].name}</h4>
+          </div>
+        </div>
+        <button
+          onClick={nextSlide}
+          className='absolute left-0 top-[50%] translate-y-[-50%] cursor-pointer p-2 text-2xl text-black/30 hover:text-black/60 md:left-5'
         >
-          {slides.map((element, index) => (
-            <img src={element} key={index} alt='' />
-          ))}
-        </div>
-        <div className='absolute inset-0 flex items-center justify-between p-4'>
-          <button onClick={prev} className='text-gray-800 rounded-full bg-white/80 p-1 shadow hover:bg-white'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='h-6 w-6'
-            >
-              <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
-            </svg>
-          </button>
-          <button onClick={next} className='text-gray-800 rounded-full bg-white/60 p-1 shadow hover:bg-white'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='h-6 w-6'
-            >
-              <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
-            </svg>
-          </button>
-        </div>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-8 w-8'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+          </svg>
+        </button>
+        <button
+          onClick={prevSlide}
+          className='absolute right-0 top-[50%] translate-y-[-50%] cursor-pointer p-2 text-2xl text-black/30 hover:text-black/60 md:right-5'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-8 w-8'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+          </svg>
+        </button>
       </div>
     </>
   )
