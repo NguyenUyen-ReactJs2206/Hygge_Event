8d8979f0af4117bc22590a3d0ea5910dce027966
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-06-25 16:47:10 +0700

    About Us Page

---
+++ EventApp/src/api/aboutUs.api.ts
@@ -0,0 +1,33 @@
+import aboutUsOne from '../assets/images/aboutUs/about-us-1.png'
+import aboutUsTwo from '../assets/images/aboutUs/about-us-2.png'
+import aboutUsThree from '../assets/images/aboutUs/about-us-3.png'
+import aboutUsFour from '../assets/images/aboutUs/about-us-4.png'
+
+export const aboutUsApi = [
+  {
+    image: `${aboutUsOne}`,
+    title: 'Overview',
+    content:
+      "Established in 2023. Hygge Weddings & Events is named after the Danish concept of hygge. Which represents the cozy and heartfelt atmosphere created by embracing life's simple pleasures."
+  },
+  {
+    image: `${aboutUsTwo}`,
+    title: 'Our Mision',
+    content:
+      'Our mission is to seamlessly blend simplicity, elegance, and atouch of enchantment to transform your cherished love dream into a breathtaking reality. We believe that your heart should guide the narrative, and we are here to orchestrate its beautiful realization.'
+  },
+  {
+    image: `${aboutUsThree}`,
+    title: 'Our Team',
+    content:
+      'Our team is composed of highly skilled and experienced professionals who have honed their expertise in the realm of Weddings & Event perfection and a commitment to excellence, we go above and beyond to surpass your expectations and deliver truly exceptional experiences.'
+  },
+  {
+    image: `${aboutUsFour}`,
+    title: 'Our Commitment',
+    contentOne:
+      'At Hygge Weddings & Events, we take on a multifaceted role that goes beyond simply fulfilling dreams. We become your confidants, effective partners, and diligent assistants throughout your journey. With meticulous attention to detail,we care for every element, ensuring an impeccable and flawless big day for our esteemed clients.',
+    contentTwo:
+      'Guided by our motto,"From Warmth to Endless Amore", we inspire passionate soulsto unleash their imagination and craft their own unique narratives. We are here to transform your vision into reality through innovative concepts and designs that reflect your individuality and style.'
+  }
+]
--- EventApp/src/pages/AboutUs/AboutUs.tsx
+++ EventApp/src/pages/AboutUs/AboutUs.tsx
@@ -1,9 +1,128 @@
-export default function AboutUs() {
+import { aboutUsApi } from 'src/api/aboutUs.api'
+
+export default function AboutU2() {
   return (
-    <div className=''>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro blanditiis ut consequuntur ratione, ab pariatur
-        laboriosam accusamus natus nemo numquam harum ducimus a veritatis neque vitae suscipit. Laudantium, modi! In?
+    <div className='about-us'>
+      <div className='pt-20 lg:pt-[70px]'>
+        <h1 className='font mb-4 pl-10 pt-6 text-4xl font-semibold tracking-wider text-gray md:hidden'>About Us</h1>
+        <div className='group relative grid grid-cols-12'>
+          <div className=' col-span-12 lg:col-span-7 xl:col-span-8'>
+            <div className='h-[300px] w-full overflow-hidden xs:h-[400px] sm:h-full'>
+              <img
+                src={aboutUsApi[0].image}
+                alt='conceptOne'
+                className='h-full w-full object-cover duration-[2000ms] group-hover:scale-110 group-hover:opacity-30 group-hover:duration-[2000ms] lg:opacity-100 lg:group-hover:opacity-100'
+              />
+            </div>
+          </div>
+          <div className='absolute col-span-12 h-full w-full lg:static lg:col-span-5 lg:bg-blue xl:col-span-4'>
+            <div className='h-full w-full items-center p-4 sm:px-10 sm:py-20 lg:px-20 lg:pt-32 '>
+              <div
+                className='translate-y-12 transform opacity-0 transition-all 
+            duration-1000 group-hover:translate-y-0
+            group-hover:opacity-100 lg:translate-y-0
+            lg:transform-none lg:opacity-100
+            lg:transition-none lg:duration-0'
+              >
+                <h3 className='text-xl font-extrabold uppercase tracking-wider text-blue sm:text-2xl sm:font-semibold lg:text-white'>
+                  {aboutUsApi[0].title}
+                </h3>
+                <p className='mt-4 text-justify text-[12px] tracking-wide xs:text-[14px] sm:text-base lg:text-white'>
+                  {aboutUsApi[0].content}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='group relative grid grid-cols-12'>
+          <div className='absolute col-span-12 h-full w-full lg:static lg:col-span-5 xl:col-span-4'>
+            <div className='h-full w-full items-center p-4 sm:px-10 sm:py-20 lg:px-20 lg:pt-32'>
+              <div
+                className='translate-y-12 transform opacity-0 transition-all 
+            duration-1000 group-hover:translate-y-0
+            group-hover:opacity-100 lg:translate-y-0
+            lg:transform-none lg:opacity-100
+            lg:transition-none lg:duration-0'
+              >
+                <h3 className='text-xl font-extrabold uppercase tracking-wider text-blue sm:text-2xl sm:font-semibold'>
+                  {aboutUsApi[1].title}
+                </h3>
+                <p className='mt-4 text-justify text-[12px] tracking-wide xs:text-[14px] sm:text-base'>
+                  {aboutUsApi[1].content}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className=' col-span-12 lg:col-span-7 xl:col-span-8'>
+            <div className='h-[300px] w-full overflow-hidden xs:h-[400px] sm:h-full'>
+              <img
+                src={aboutUsApi[1].image}
+                alt='conceptOne'
+                className='h-full w-full object-cover duration-[2000ms] group-hover:scale-110 group-hover:opacity-30 group-hover:duration-[2000ms] lg:group-hover:opacity-100'
+              />
+            </div>
+          </div>
+        </div>
+        <div className='group relative grid grid-cols-12'>
+          <div className=' col-span-12 lg:col-span-7 xl:col-span-8'>
+            <div className='h-[300px] w-full overflow-hidden xs:h-[400px] sm:h-full'>
+              <img
+                src={aboutUsApi[2].image}
+                alt='conceptOne'
+                className='h-full w-full object-cover duration-[2000ms] group-hover:scale-110 group-hover:opacity-30 group-hover:duration-[2000ms] lg:opacity-100 lg:group-hover:opacity-100'
+              />
+            </div>
+          </div>
+          <div className='absolute col-span-12 h-full w-full lg:static lg:col-span-5 xl:col-span-4'>
+            <div className='h-full w-full items-center p-4 sm:px-10 sm:py-20 lg:bg-blue lg:px-20 lg:pt-32 '>
+              <div
+                className='translate-y-12 transform opacity-0 transition-all 
+            duration-1000 group-hover:translate-y-0
+            group-hover:opacity-100 lg:translate-y-0
+            lg:transform-none lg:opacity-100
+            lg:transition-none lg:duration-0'
+              >
+                <h3 className='text-xl font-extrabold uppercase tracking-wider text-blue sm:text-2xl sm:font-semibold lg:text-white'>
+                  {aboutUsApi[2].title}
+                </h3>
+                <p className='mt-4 text-justify text-[12px] tracking-wide xs:text-[14px] sm:text-base lg:text-white'>
+                  {aboutUsApi[2].content}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='group relative grid grid-cols-12'>
+          <div className='absolute col-span-12 h-full w-full lg:static lg:col-span-5 xl:col-span-4'>
+            <div className='h-full w-full items-center p-4 sm:px-10 sm:py-20 lg:px-20 lg:pt-32 '>
+              <div
+                className='translate-y-12 transform opacity-0 transition-all 
+            duration-1000 group-hover:translate-y-0
+            group-hover:opacity-100 lg:translate-y-0
+            lg:transform-none lg:opacity-100
+            lg:transition-none lg:duration-0'
+              >
+                <h3 className='text-xl font-extrabold uppercase tracking-wider text-blue sm:text-2xl sm:font-semibold'>
+                  {aboutUsApi[3].title}
+                </h3>
+                <p className='mt-4 text-justify text-[12px] tracking-wide xs:text-[14px] sm:text-base'>
+                  {aboutUsApi[3].contentOne}
+                  <br />
+                  {aboutUsApi[3].contentTwo}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='col-span-12 lg:col-span-7 xl:col-span-8'>
+            <div className='h-[300px] w-full overflow-hidden xs:h-[400px] sm:h-full'>
+              <img
+                src={aboutUsApi[3].image}
+                alt='conceptOne'
+                className='h-full w-full object-cover duration-[2000ms] group-hover:scale-110 group-hover:opacity-30 group-hover:duration-[2000ms] lg:group-hover:opacity-100'
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
