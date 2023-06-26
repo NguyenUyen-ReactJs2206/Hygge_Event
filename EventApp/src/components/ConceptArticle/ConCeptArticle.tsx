037ca5589e7d8fed96cb428f871ae335ff57cd9f
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-06-27 01:09:57 +0700

    Images Api for Concept Detail

--- EventApp/src/components/ConceptArticle/ConCeptArticle.tsx
+++ EventApp/src/components/ConceptArticle/ConCeptArticle.tsx
@@ -29,9 +29,11 @@ export default function ConCeptArticle({ name, pathConcept }: Props) {
             <h3 className='cursor-default text-3xl font-medium tracking-wide text-blue md:text-3xl md:font-medium'>
               {name}
             </h3>
-            <button className='underline-effect hover:underline-effect text-base text-gray duration-300 hover:font-medium hover:text-blue/90 hover:duration-300'>
-              Read More
-            </button>
+            <Link to={pathConcept}>
+              <button className='underline-effect hover:underline-effect text-base text-gray duration-300 hover:font-medium hover:text-blue/90 hover:duration-300'>
+                Read More
+              </button>
+            </Link>
           </div>
         </div>
       </div>
