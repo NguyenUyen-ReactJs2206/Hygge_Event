8a2e3d3e606d34df625acffba12ee32770ce528f
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-07-07 07:27:35 +0700

    Transition Banner

--- EventApp/src/pages/Home/components/CarouselBanner/CarouselBanner.tsx
+++ EventApp/src/pages/Home/components/CarouselBanner/CarouselBanner.tsx
@@ -1,49 +1,31 @@
-import { useEffect, useState } from 'react'
+import Carousel from 'react-multi-carousel'
 import { banners } from 'src/api/home.api'
 
 export default function CarouselBanner() {
-  const autoSlide = true
-  const autoSlideInterval = 3000
-
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  const nextSlide = () => {
-    setCurrentIndex((curr) => (curr === banners.length - 1 ? 0 : curr + 1))
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 0 },
+      items: 1,
+      slidesToSlide: 1
+    }
   }
-
-  const goToSlide = (slideIndex: number) => {
-    setCurrentIndex((curr) => (curr === slideIndex ? banners.length - (banners.length - slideIndex) : slideIndex))
-  }
-
-  useEffect(() => {
-    if (!autoSlide) return
-    const slideInterval = setInterval(nextSlide, autoSlideInterval)
-    return () => clearInterval(slideInterval)
-  }, [autoSlide])
   return (
     <div className=''>
-      <div className='group h-full w-full'>
-        <div className='relative flex h-full w-full justify-center text-center transition-all duration-1000 ease-out'>
-          <div className='h-[75vh] w-full overflow-hidden md:h-[90vh] lg:h-[100vh]'>
-            <img
-              src={banners[currentIndex]}
-              alt={banners[currentIndex]}
-              className=' h-full w-full object-none md:object-cover'
-            />
+      <Carousel
+        responsive={responsive}
+        pauseOnHover={false}
+        autoPlay={true}
+        infinite={true}
+        autoPlaySpeed={2000}
+        showDots={true}
+      >
+        {banners.map((image, index) => (
+          <div key={index} className='h-[100vh] w-full overflow-hidden'>
+            <img src={image} alt={image} className=' h-full w-full object-none md:object-cover' />
           </div>
-          <div className='absolute bottom-0 left-0 right-0 flex h-10 items-center justify-end gap-2 md:justify-center'>
-            {banners.map((_, index) => (
-              <button
-                key={index}
-                className={`
-          z-10 h-2 w-2 rounded-full bg-white transition-all md:h-3 md:w-3
-          ${currentIndex === index ? '' : 'bg-opacity-30'}`}
-                onClick={() => goToSlide(index)}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
+        ))}
+      </Carousel>
     </div>
   )
 }
