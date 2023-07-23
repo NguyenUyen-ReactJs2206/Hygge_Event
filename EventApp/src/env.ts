5f5e26b3756117b7a62df2007aea319e54c7ca70
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-07-23 19:21:51 +0700

    fix deploy

---
+++ EventApp/src/env.ts
@@ -0,0 +1,5 @@
+export const YOUR_EMAIL_SERVICE_ID = 'service_iutjy04'
+export const YOUR_EMAIL_TEAMPLATE_ID_SERVER = 'template_puem5fb'
+export const YOUR_PUBLIC_KEY_ID = '1LgTYIVsZpNjl66zG'
+
+export const YOUR_EMAIL_TEAMPLATE_ID_CUSTOMER = 'template_rjdb1gl'
